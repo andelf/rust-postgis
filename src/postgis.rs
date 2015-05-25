8cbeca6c49c756f45c5ebfea72b2e96c0a3b8bb4
andelf <andelf@gmail.com>
2015-05-26 01:19:17 +0800

    add Polygon

--- src/postgis.rs
+++ src/postgis.rs
@@ -11,31 +11,43 @@ use byteorder::{ReadBytesExt, WriteBytesExt, BigEndian, LittleEndian};
 
 
 trait Geometry: fmt::Debug + Sized {
+    type PointType: ToPoint;
     #[inline(always)]
     fn type_id() -> u32;
 
-    #[inline(always)]
-    fn has_z() -> bool {
-        false
+    fn read_ewkb<R: Read>(raw: &mut R) -> byteorder::Result<Self> {
+        let byte_order = try!(raw.read_i8());
+        let is_be = byte_order == 0i8;
+
+        let type_id = try!(read_u32(raw, is_be));
+        if type_id != Self::type_id() {
+            return Err(byteorder::Error::UnexpectedEOF);
+        }
+
+        match Self::PointType::opt_srid() {
+            Some(srid) => {
+                if try!(read_i32(raw, is_be)) != srid {
+                    return Err(byteorder::Error::UnexpectedEOF);
+                }
+            },
+            _ => ()
+        }
+        Self::read_ewkb_body(raw, is_be)
     }
 
-    #[inline(always)]
-    fn has_m() -> bool {
-        false
-    }
+    fn read_ewkb_body<R: Read>(raw: &mut R, is_be: bool) -> byteorder::Result<Self>;
 
-    #[inline(always)]
-    fn has_srid() -> bool {
-        false
+    fn write_ewkb<W: Write+?Sized>(&self, _: &Type, w: &mut W) -> Result<IsNull> {
+        // use LE
+        try!(w.write_u8(0x01));
+        let mut type_id = Self::type_id();
+        w.write_u32::<LittleEndian>(type_id);
+        Self::PointType::opt_srid().map(|srid| w.write_i32::<LittleEndian>(srid));
+        self.write_ewkb_body(w);
+        Ok(IsNull::No)
     }
-
-    #[inline]
     fn write_ewkb_body<W: Write+?Sized>(&self, w: &mut W) -> Result<()>;
 
-    #[inline]
-    fn read_ewkb_body() {
-
-    }
 }
 
 fn read_u32<R: Read>(raw: &mut R, is_be: bool) -> byteorder::Result<u32> {
@@ -81,11 +93,6 @@ impl SRID for NoSRID {
 }
 
 
-pub trait PointConstructor: Sized {
-    fn new() -> Self {
-        unsafe { mem::zeroed() }
-    }
-}
 trait ToPoint: Sized {
     type SRIDType: SRID;
 
@@ -104,6 +111,12 @@ trait ToPoint: Sized {
     }
     fn opt_srid() -> Option<i32> {
         Self::SRIDType::as_srid()
+    }
+    fn x(&self) -> f64 {
+        unsafe { *mem::transmute::<_, *const f64>(self) }
+    }
+    fn y(&self) -> f64 {
+        unsafe { *mem::transmute::<_, *const f64>(self).offset(1) }
     }
     fn opt_z(&self) -> Option<f64> {
         None
@@ -138,7 +151,6 @@ trait ToPoint: Sized {
 
     fn read_ewkb_body<R: Read>(raw: &mut R, is_be: bool) -> byteorder::Result<Self> {
         let x = try!(read_f64(raw, is_be));
-        println!("llll => {}", x);
         let y = try!(read_f64(raw, is_be));
         let z = if Self::has_z() {
             Some(try!(read_f64(raw, is_be)))
@@ -164,8 +176,8 @@ trait ToPoint: Sized {
 
     fn write_ewkb_body<W: Write+?Sized>(&self, w: &mut W) -> Result<()> {
         // lol
-        let x = unsafe { *mem::transmute::<_, *const f64>(&self) };
-        let y = unsafe { *mem::transmute::<_, *const f64>(&self).offset(1) };
+        let x = unsafe { *mem::transmute::<_, *const f64>(self) };
+        let y = unsafe { *mem::transmute::<_, *const f64>(self).offset(1) };
         w.write_f64::<LittleEndian>(x);
         w.write_f64::<LittleEndian>(y);
         self.opt_z().map(|z| w.write_f64::<LittleEndian>(z));
@@ -195,12 +207,24 @@ pub struct Point<S: SRID = WGS84> {
     phantom: PhantomData<S>
 }
 
+impl Point {
+    pub fn new(x: f64, y: f64) -> Point {
+        Point { x: x, y: y, phantom: PhantomData }
+    }
+}
+
 #[derive(Copy, Clone)]
 pub struct PointZ<S: SRID = WGS84> {
     pub x: f64,
     pub y: f64,
     pub z: f64,
     phantom: PhantomData<S>
+}
+
+impl PointZ {
+    pub fn new(x: f64, y: f64, z: f64) -> PointZ {
+        PointZ { x: x, y: y, z: z, phantom: PhantomData }
+    }
 }
 
 #[derive(Copy, Clone)]
@@ -211,6 +235,12 @@ pub struct PointM<S: SRID = WGS84> {
     phantom: PhantomData<S>
 }
 
+impl PointM {
+    pub fn new(x: f64, y: f64, m: f64) -> PointM {
+        PointM { x: x, y: y, m: m, phantom: PhantomData }
+    }
+}
+
 #[derive(Copy, Clone)]
 pub struct PointZM<S: SRID = WGS84> {
     pub x: f64,
@@ -218,6 +248,12 @@ pub struct PointZM<S: SRID = WGS84> {
     pub z: f64,
     pub m: f64,
     phantom: PhantomData<S>
+}
+
+impl PointZM {
+    pub fn new(x: f64, y: f64, z: f64, m: f64) -> PointZM {
+        PointZM { x: x, y: y, z: z, m: z, phantom: PhantomData }
+    }
 }
 
 impl<S: SRID> ToPoint for Point<S> {
@@ -280,6 +316,7 @@ macro_rules! accepts_geography {
     )
 }
 
+
 macro_rules! impl_traits_for_point {
     ($ptype:ident) => (
         impl FromSql for $ptype {
@@ -297,7 +334,6 @@ macro_rules! impl_traits_for_point {
             }
         }
 
-        impl PointConstructor for $ptype {}
         impl fmt::Display for $ptype {
             fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
                 try!(write!(f, "{}", self.describ()));
@@ -335,8 +371,110 @@ impl<P: ToPoint> LineString<P> {
     }
 }
 
+impl<P: ToPoint> fmt::Display for LineString<P> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        try!(write!(f, "LINESTRING"));
+        if P::has_z() {
+            try!(f.write_str("Z"));
+        }
+        if P::has_m() {
+            try!(f.write_str("M"));
+        }
+        try!(f.write_str("("));
+        for (i, point) in self.points.iter().enumerate() {
+            if i >= 1 {
+                try!(write!(f, ","));
+            }
+            try!(write!(f, "{} {}", point.x(), point.y()));
+            point.opt_z().map(|z| write!(f, " {}", z));
+            point.opt_m().map(|m| write!(f, " {}", m));
+        }
+        try!(write!(f, ")"));
+        Ok(())
+    }
+}
+
+
+impl<P: ToPoint + fmt::Debug> Geometry for LineString<P> {
+    type PointType = P;
+    fn type_id() -> u32 {
+        let type_id = P::type_id();
+        (type_id & 0xffff_ff00) | 0x0000_0002
+    }
+
+    fn read_ewkb_body<R: Read>(raw: &mut R, is_be: bool) -> byteorder::Result<Self> {
+        let mut ret = LineString::new();
+        let size = try!(read_u32(raw, is_be)) as usize;
+        for _ in 0..size {
+            ret.points.push(P::read_ewkb_body(raw, is_be).unwrap())
+        }
+        Ok(ret)
+    }
+
+    fn write_ewkb_body<W: Write+?Sized>(&self, w: &mut W) -> Result<()> {
+        try!(w.write_u32::<LittleEndian>(self.points.len() as u32));
+        for point in self.points.iter() {
+            point.write_ewkb_body(w);
+        }
+        Ok(())
+    }
+
+}
 
 impl<P: ToPoint + fmt::Debug> ToSql for LineString<P> {
+    to_sql_checked!();
+    accepts_geography!();
+    fn to_sql<W: Write+?Sized>(&self, ty: &Type, w: &mut W) -> Result<IsNull> {
+        self.write_ewkb(ty, w)
+    }
+
+}
+impl<P: ToPoint + fmt::Debug> FromSql for LineString<P> {
+    accepts_geography!();
+    fn from_sql<R: Read>(ty: &Type, raw: &mut R) -> Result<LineString<P>> {
+        <Self as Geometry>::read_ewkb(raw).map_err(|_| Error::WrongType(ty.clone()))
+    }
+}
+
+#[derive(Debug)]
+pub struct Polygon<P> {
+    pub rings: Vec<LineString<P>>
+}
+
+
+impl<P: ToPoint> Polygon<P> {
+    pub fn new() -> Polygon<P> {
+        Polygon { rings: Vec::new() }
+    }
+}
+
+impl<P: ToPoint + fmt::Debug> Geometry for Polygon<P> {
+    type PointType = P;
+    fn type_id() -> u32 {
+        let type_id = P::type_id();
+        (type_id & 0xffff_ff00) | 0x0000_0003
+    }
+
+    fn read_ewkb_body<R: Read>(raw: &mut R, is_be: bool) -> byteorder::Result<Self> {
+        let mut ret = Polygon::new();
+        let size = try!(read_u32(raw, is_be)) as usize;
+        for _ in 0..size {
+            ret.rings.push(LineString::<P>::read_ewkb_body(raw, is_be).unwrap())
+        }
+        Ok(ret)
+    }
+
+    fn write_ewkb_body<W: Write+?Sized>(&self, w: &mut W) -> Result<()> {
+        try!(w.write_u32::<LittleEndian>(self.rings.len() as u32));
+        for ring in self.rings.iter() {
+            ring.write_ewkb_body(w);
+        }
+        Ok(())
+    }
+
+}
+
+impl<P: ToPoint + fmt::Debug> ToSql for Polygon<P> {
     to_sql_checked!();
     accepts_geography!();
     fn to_sql<W: Write+?Sized>(&self, _: &Type, w: &mut W) -> Result<IsNull> {
@@ -348,18 +486,17 @@ impl<P: ToPoint + fmt::Debug> ToSql for LineString<P> {
         type_id |= 0x0000_0002;
         w.write_u32::<LittleEndian>(type_id);
         P::opt_srid().map(|srid| w.write_i32::<LittleEndian>(srid));
-        for point in self.points.iter() {
+        for point in self.rings.iter() {
             point.write_ewkb_body(w);
         }
         Ok(IsNull::No)
     }
 
 }
-
-impl<P: ToPoint + fmt::Debug> FromSql for LineString<P> {
+impl<P: ToPoint + fmt::Debug> FromSql for Polygon<P> {
     accepts_geography!();
-    fn from_sql<R: Read>(ty: &Type, raw: &mut R) -> Result<LineString<P>> {
-        let mut ret = LineString::new();
+    fn from_sql<R: Read>(ty: &Type, raw: &mut R) -> Result<Polygon<P>> {
+        let mut ret = Polygon::new();
         let byte_order = try!(raw.read_i8());
         let is_be = byte_order == 0i8;
 
@@ -381,9 +518,8 @@ impl<P: ToPoint + fmt::Debug> FromSql for LineString<P> {
         println!("size => size {}", size);
         for _ in 0..size {
             println!("");
-            ret.points.push(P::read_ewkb_body(raw, is_be).unwrap())
+            ret.rings.push(LineString::<P>::read_ewkb_body(raw, is_be).unwrap())
         }
         Ok(ret)
     }
-
 }
